f193f9c652c1ac7595a94f9ccf2256fcec13157b
Sébastien Dudek <fluxius@gmail.com>
2025-05-11 11:51:09 +0200

    Update misc_utils_python.cc

--- gnuradio/gr-droneid/python/droneid/bindings/misc_utils_python.cc
+++ gnuradio/gr-droneid/python/droneid/bindings/misc_utils_python.cc
@@ -18,7 +18,6 @@
 /***********************************************************************************/
 
 #include <pybind11/complex.h>
-#include <pybind11/cstdint.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 
